c7df62014030ada0e6e1cae4a50ff032c43307c0
Spencer Lustila <spencerwlustila@gmail.com>
2026-04-17 14:25:58 -0400

    finished info for pricings. need to make them side by side instead of on top of eachother for each section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,23 +10,23 @@ export default function AboutPage() {
         Caribou Accounting is a family-run firm focused on providing reliable tax preparation and bookkeeping services for individuals and small businesses.
       </p>
 
-      <p className="text-md mb-3">
+      <p className="text-md text-[var(--secondary)] mb-3">
         With experience as a former H&R Block tax associate, we bring a strong foundation in tax preparation along with a practical, 
         client-focused approach. We understand that every situation is different, and we work closely with clients to ensure their 
         taxes and financials are handled accurately and efficiently.      
         </p>
 
-      <p className="text-md mb-3">
+      <p className="text-md text-[var(--secondary)] mb-3">
         We offer both <span className="font-bold" >virtual appointments and in-person support</span>, making it easy to work with us in a way that fits your schedule. As we 
         continue to grow, we plan to expand into a dedicated storefront location to better serve our local community.
       </p>
 
-      <p className="text-md mb-3">
-        Our approach is simple: clear communication, transparent pricing, and dependable service. Whether you need help filing your taxes or managing your books year-round, 
+      <p className="text-md text-[var(--secondary)] mb-3">
+        Our approach is simple: clear communication, transparent pricing and dependable service. Whether you need help filing your taxes or managing your books year-round, 
         Caribou Accounting is here to provide straightforward, reliable support.
       </p>
 
-      <p className="text-md mb-3">
+      <p className="text-md text-[var(--secondary)] mb-3">
         We take pride in building long-term relationships with our clients and being a trusted resource year after year.
       </p>
 
---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function ContactPage() {
+    return(
+   <main className="min-h-screen justify-start pt-25 bg-white text-black p-6">   
+     <h1 className="text-md text-[var(--secondary)]">
+        gracie is a weenie lol
+    </h1>
+    </main>
+    )
+}
