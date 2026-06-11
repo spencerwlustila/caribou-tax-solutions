2335efa04e11906789666205e3b3b5ca60e362fa
Spencer Lustila <spencerwlustila@gmail.com>
2026-06-11 11:38:41 -0400

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -84,6 +85,7 @@ export default function RootLayout({
 </p>
   </div>
 </footer>
+<Analytics />
 </body>
     </html>
   );
